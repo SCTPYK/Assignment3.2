4f7923ac578a23b5be50ad3da927ad53e02d98c0
SCTPYK <xoxdx49@gmail.com>
2025-04-06 14:38:05 +0800

    lifecycle config

--- main.tf
+++ main.tf
@@ -103,6 +103,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "s3-lifecycle" {
     id     = "Send to Glacier after 30 days"
     status = "Enabled"
 
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+
     filter {
       prefix = ""
     }
