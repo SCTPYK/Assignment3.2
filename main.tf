76e6d6049994a46dbe63b8324942b0ca281a21dc
SCTPYK <xoxdx49@gmail.com>
2025-04-06 13:59:14 +0800

    provider comes later

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce8-tfstate-unique"
-    key    = "yk-s3-tf-ci.tfstate"  
+    key    = "yk-s3-tf-ci.tfstate"
     region = "us-east-1"
   }
 }
