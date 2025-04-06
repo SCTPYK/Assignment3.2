77681c92aed9d6e7b65df18ff8d32775cf06beb5
SCTPYK <xoxdx49@gmail.com>
2025-04-06 15:16:59 +0800

    loggin access

--- main.tf
+++ main.tf
@@ -319,7 +319,7 @@ resource "aws_s3_bucket" "s3_tf-log_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.log_bucket.id
+  bucket = aws_s3_bucket.s3_tf-log_bucket.id
   acl    = "log-delivery-write"
 }
 
