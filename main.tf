2ba7734656e8dca67a975b3fbb227edf9b69ed8f
SCTPYK <xoxdx49@gmail.com>
2025-04-06 12:52:41 +0800

    aws make s3 name unique

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "sctp-ce8-tfstate"
+    bucket = "sctp-ce8-tfstate-unique"
     key    = "yk-s3-tf-ci.tfstate"
     region = "us-east-1"
   }
