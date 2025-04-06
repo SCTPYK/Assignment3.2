c65be396b49bf264f948b679e0ef7add9a566993
SCTPYK <xoxdx49@gmail.com>
2025-04-06 13:50:34 +0800

    join

--- main.tf
+++ main.tf
@@ -26,5 +26,5 @@ locals {
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+  bucket = join("-", [local.name_prefix, "s3-tf-bkt", local.account_id])
 }
