26124b99547e61acdb525e4d4506632579aab151
SCTPYK <xoxdx49@gmail.com>
2025-04-06 13:01:30 +0800

    Fix syntax for provider block

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
   required_providers {
     aws = {
@@ -9,6 +5,10 @@ terraform {
       version = "~> 3.0"  
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 terraform {
