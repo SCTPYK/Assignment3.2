c9bdd8ebb9d04a4965c7271f7fd3deaa8ebc09be
SCTPYK <xoxdx49@gmail.com>
2025-04-06 14:45:27 +0800

    replication config

--- main.tf
+++ main.tf
@@ -117,3 +117,107 @@ resource "aws_s3_bucket_lifecycle_configuration" "s3-lifecycle" {
   }
 }
 
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "replication" {
+  name               = "tf-iam-role-replication-s3"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "replication" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetReplicationConfiguration",
+      "s3:ListBucket",
+    ]
+
+    resources = [aws_s3_bucket.s3_tf.arn]
+  }
+    statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionTagging",
+    ]
+
+    resources = ["${aws_s3_bucket.s3_tf.arn}/*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ReplicateObject",
+      "s3:ReplicateDelete",
+      "s3:ReplicateTags",
+    ]
+
+    resources = ["${aws_s3_bucket.s3_tf.arn}/*"]
+  }
+}
+
+resource "aws_iam_policy" "replication" {
+  name   = "tf-iam-role-policy-replication-s3"
+  policy = data.aws_iam_policy_document.replication.json
+}
+
+resource "aws_iam_role_policy_attachment" "replication" {
+  role       = aws_iam_role.replication.name
+  policy_arn = aws_iam_policy.replication.arn
+}
+
+resource "aws_s3_bucket" "destination" {
+  bucket = "tf-test-bucket-destination-s3"
+}
+
+resource "aws_s3_bucket" "source" {
+  provider = aws.central
+  bucket   = "tf-test-bucket-source-12345"
+}
+
+resource "aws_s3_bucket_acl" "source_bucket_acl" {
+  provider = aws.central
+
+  bucket = aws_s3_bucket.source.id
+  acl    = "private"
+}
+
+
+resource "aws_s3_bucket_replication_configuration" "replication" {
+  provider = aws.central
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.s3-versioning]
+
+  role   = aws_iam_role.replication.arn
+  bucket = aws_s3_bucket.s3_tf.id
+
+  rule {
+    id = "replication-config"
+
+    filter {
+      prefix = ""
+    }
+
+    status = "Enabled"
+
+    destination {
+      bucket        = aws_s3_bucket.s3_tf.arn
+      storage_class = "STANDARD"
+    }
+  }
+}
