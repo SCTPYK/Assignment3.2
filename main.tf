adffb94911067aceb8643ef49b5783b11d79fe16
SCTPYK <xoxdx49@gmail.com>
2025-04-06 12:58:49 +0800

    Fix interpolation and provider

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "aws" {
 }
 
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"  
+    }
+  }
+}
+
+terraform {
   backend "s3" {
     bucket = "sctp-ce8-tfstate-unique"
     key    = "yk-s3-tf-ci.tfstate"
@@ -13,10 +22,10 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
+  name_prefix = split("/", "$data.aws_caller_identity.current.arn")[1]
   account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+  bucket = "$local.name_prefix-s3-tf-bkt-$local.account_id"
 }
